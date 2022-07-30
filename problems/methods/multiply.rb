2344d07782c672c899087414bb310f9a7ea0675a
ryanneilparker <ryanneilparker@gmail.com>
2022-07-30 10:49:51 +0000

    Add  multiply.rb

---
+++ problems/methods/multiply.rb
@@ -0,0 +1,9 @@
+=begin
+Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+=end
+
+def multiply(a, b)
+  a * b
+end
+
+puts multiply(2, 4)
