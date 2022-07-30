b374aceab4badfef776d2dca592d2e9c46e87405
ryanneilparker <ryanneilparker@gmail.com>
2022-07-30 10:46:06 +0000

    Add method.rb

---
+++ problems/methods/method.rb
@@ -0,0 +1,9 @@
+=begin
+Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string.
+=end 
+
+def greeting(name)
+  puts "Hello, #{name}!"
+end
+
+greeting("Ryan")
