a34c417727f43437bf3665e01f295b6f168d9ac4
ryanneilparker <ryanneilparker@gmail.com>
2022-07-30 10:01:51 +0000

    Update name.rb

--- problems/variables/name.rb
+++ problems/variables/name.rb
@@ -6,3 +6,11 @@ puts "Please enter your name below:"
 name = gets.chomp
 
 puts "Thank you #{name}, welcome to Ruby!"
+
+=begin
+Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row. Hint: you can use the times method to do something repeatedly.
+=end
+
+10.times do |x|
+  puts name
+end
