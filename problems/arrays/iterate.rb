07685c52b294c0e57d31a16d990a7a91c6f43b1d
ryanneilparker <ryanneilparker@gmail.com>
2022-08-03 09:00:53 +0000

    Add iterate.rb

---
+++ problems/arrays/iterate.rb
@@ -0,0 +1,8 @@
+# Write a program that iterates over an array and builds a new array that is the result of incrementing each value in the original array by a value of 2. You should have two arrays at the end of this program, The original array and the new array you've created. Print both arrays to the screen using the p method instead of puts.
+
+old = [1, 2, 3]
+
+new = old.map { |n| n + 2 }
+
+p old
+p new
