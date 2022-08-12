4a6b53d77973e786c72ca065df0ef4db9373521e
ryanneilparker <ryanneilparker@gmail.com>
2022-08-12 11:32:02 +0000

    Add until.rb

---
+++ problems/exercises/until.rb
@@ -0,0 +1,10 @@
+# Given the array of several numbers below, use an until loop to print each number.
+
+numbers = [7, 9, 13, 25, 18]
+
+i = 0
+
+until i == numbers.length()
+    puts numbers[i]
+    i += 1
+end
