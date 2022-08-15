6d3cafc6f8e72fda3fc81e8350bc132401723bc2
ryanneilparker <ryanneilparker@gmail.com>
2022-08-15 20:12:49 +0000

    Update breakfast.rb

--- problems/return/breakfast.rb
+++ problems/return/breakfast.rb
@@ -5,3 +5,5 @@ sun = ['visible', 'hidden'].sample
 # Write an if statement that prints "The sun is so bright!" if sun equals 'visible'.
 
 puts "The sun is so bright!" if sun == 'visible'
+
+puts "The clouds are blocking the sun!" unless sun =='visible'
