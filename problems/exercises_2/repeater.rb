49c5a882845ba4fbbe6f356737d3c91364664eed
ryanneilparker <ryanneilparker@gmail.com>
2022-08-15 18:26:24 +0000

    Add repeater.rb

---
+++ problems/exercises_2/repeater.rb
@@ -0,0 +1,5 @@
+# Write a program that asks the user to type something in, after which your program should simply display what was entered.
+
+puts "Please enter some text: "
+input = gets.chomp()
+puts input
