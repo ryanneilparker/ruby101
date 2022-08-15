b8087b715f161c8df32e9690c5ac1fb7edc9e679
ryanneilparker <ryanneilparker@gmail.com>
2022-08-15 20:11:00 +0000

    Add breakfast.rb

---
+++ problems/return/breakfast.rb
@@ -0,0 +1,7 @@
+# In the code below, sun is randomly assigned as 'visible' or 'hidden'.
+
+sun = ['visible', 'hidden'].sample
+
+# Write an if statement that prints "The sun is so bright!" if sun equals 'visible'.
+
+puts "The sun is so bright!" if sun == 'visible'
