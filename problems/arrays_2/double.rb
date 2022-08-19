f409732fb73ce13f335d00090108901d520e0fa1
ryanneilparker <ryanneilparker@gmail.com>
2022-08-19 13:54:00 +0000

    Add doubled.rb

---
+++ problems/arrays_2/double.rb
@@ -0,0 +1,8 @@
+# In the code below, an array containing the numbers 1 through 5 is assigned to numbers.
+
+numbers = [1, 2, 3, 4, 5]
+# Use Array#map to iterate over numbers and return a new array with each number doubled. 
+# Assign the returned array to a variable named doubled_numbers and print its value using #p
+
+doubled_numbers = numbers.map {|number| 2 * number}
+p doubled_numbers
