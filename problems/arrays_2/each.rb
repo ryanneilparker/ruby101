9804879bed428a0d13d7ada913e032b4beb1b90c
ryanneilparker <ryanneilparker@gmail.com>
2022-08-19 13:40:47 +0000

    Add each.rb

---
+++ problems/arrays_2/each.rb
@@ -0,0 +1,7 @@
+# In the code below, an array containing different types of colors is assigned to colors.
+
+colors = ['red', 'yellow', 'purple', 'green']
+
+# Use Array#each to iterate over colors and print each element.
+
+colors.each {|color| puts "I'm the color #{color}!"}
